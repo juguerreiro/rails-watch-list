c417f136b0c1b68e4a53d35eb3873f3d6eb3d48c
Julia Guerreiro <juguerreiro123@gmail.com>
2022-02-24 16:45:37 -0300

    create the index html for list

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'movies', to: 'movies#index'
-
+  resources :lists, only: [:index, :show, :new, :create]
 end
+
+# A user can see all the lists
+# A user can see the details of a given list and its name
+# A user can create a new list
